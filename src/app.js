e6f1b18d58b500af6698b18913b393218e669eb7
Chris Mursec <christophermursec@gmail.com>
2020-08-12 20:25:43 +0100

    cors

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ import mongoose from 'mongoose';
 import subscriptionRoute from './routes/subscriptions';
 import userRoute from './routes/users';
 import statisticsRoute from './routes/user-statistics';
+import cors from 'cors';
 const app = express();
 require('dotenv').config();
 
 app.use(express.json());
-
+app.use(cors({ origin: true, credentials: true }));
 mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
 	console.log('Connected to mongo')
 );
